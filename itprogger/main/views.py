591f3ab6661681c4cfb6603ba045f803b7e78496
Dmytro Niehrii <dmytro.niehrii@webbfontaine.com>
2023-08-23 16:14:28 +0300

    Sending data from view method to template added

--- itprogger/main/views.py
+++ itprogger/main/views.py
@@ -3,7 +3,16 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return render(request, 'main/index.html')
+    data = {
+        'title': 'Главная страница',
+        'values': ['Some', 'Hello', 123],
+        'obj': {
+            'car': 'BMW',
+            'age': 23,
+            'hobby': 'Football'
+        }
+    }
+    return render(request, 'main/index.html', data)
 
 def about(request):
     return render(request, 'main/about.html')
